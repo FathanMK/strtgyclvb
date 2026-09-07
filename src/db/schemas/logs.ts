e7a20952eabbbd7ae9c243a64df467cb9bbb1f40
FathanMK <emailfathanmk@gmail.com>
2026-09-07 13:19:32 +0700

    feat: add dota 2 heroes schema and rls in logs

--- src/db/schemas/logs.ts
+++ src/db/schemas/logs.ts
@@ -1,13 +1,18 @@
 import { ContentLogType } from "@/types";
 import { jsonb, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { readAllPublic } from "../policies";
 
-export const logs = pgTable("logs", {
-  id: serial("id").primaryKey(),
-  title: text("title"),
-  type: text("type"),
-  content: jsonb("content").$type<ContentLogType[]>(),
-  createdAt: timestamp("created_at", {
-    withTimezone: true,
-    mode: "string",
-  }).defaultNow(),
-});
+export const logs = pgTable(
+  "logs",
+  {
+    id: serial("id").primaryKey(),
+    title: text("title"),
+    type: text("type"),
+    content: jsonb("content").$type<ContentLogType[]>(),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+      mode: "string",
+    }).defaultNow(),
+  },
+  () => [readAllPublic],
+);
---
+++ src/features/dota-2/db/schemas/heroes/heroes.ts
@@ -0,0 +1,18 @@
+import { readAllPublic } from "@/db/policies";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const dota2Heroes = pgTable(
+  "dota_2_heroes",
+  {
+    id: serial("id").primaryKey(),
+    heroId: text("hero_id").notNull().unique(),
+    name: text("name"),
+    fullName: text("full-name"),
+    descriptions: text("descriptions").array(),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+      mode: "string",
+    }).defaultNow(),
+  },
+  () => [readAllPublic],
+);
