1aa5ef5cb25c24d0d0dd3d8fd3825c205e9c02da
FathanMK <emailfathanmk@gmail.com>
2026-09-08 17:47:59 +0700

    feat: add cloudflare services

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    loader: "custom",
+    loaderFile: "./src/utils/cloudflare-r2-loader.ts",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-687eca05b97b49c3b733ed02bcc0e690.r2.dev", // Your R2 custom domain
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
